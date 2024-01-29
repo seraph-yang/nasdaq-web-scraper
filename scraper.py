6e057e408c913afeca2bf86c413ceca5900d7c4a
seraph <106294171+seraph-yang@users.noreply.github.com>
2024-01-28 20:52:22 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -45,7 +45,7 @@ if ticker != "":
     print(url)
 
     data = requests.get(url, headers={'User-Agent': 'Custom'}, params=payload)
-    soup = BeautifulSoup(data.text, 'html.parser')
+    soup = BeautifulSoup(data.text, 'lxml')
 
     history = soup.find('table')
     history_table = pd.read_html(str(history))
